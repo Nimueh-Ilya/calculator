2c594f75cfd33237f8dd751fc7af0a0060d7a4e9
Aymen Loudiy <123456789+Nimueh-Ilya@users.noreply.github.com>
2023-10-03 18:54:57 +0100

    Fin Change calculator colors (sorry if it looks bad I'm color blind.) Remove dot and pos-neg buttons Finish screen display and calculation logic

--- index.js
+++ index.js
@@ -10,17 +10,19 @@ function multi(num1,num2) {
 function division(num1,num2) {
     return num1/num2
 }
-function operate(num1,num2,operator) {
-    if (operator == "+") {
+function operate(num1,num2) {
+    if (operation == "+") {
+        num1 = Number(num1)
+        num2 = Number(num2)
         return add(num1,num2)
     }
-    else if (operator == "-") {
+    else if (operation == "-") {
         return subs(num1,num2)
     }
-    else if (operator == "*") {
+    else if (operation == "*") {
         return multi(num1,num2)
     }
-    else if (operator == "/") {
+    else if (operation == "/") {
         return division(num1,num2)
     }
 }
@@ -28,6 +30,8 @@ const screen = document.querySelector('.screen')
 const buttons = document.querySelectorAll('.buttons')
 const numbers = document.querySelectorAll('.number')
 const operators = document.querySelectorAll('.operator')
+const equals = document.querySelector('.equal-sign')
+const clear = document.querySelector('.clear')
 let firstNumber = ""
 let secondNumber = ""
 let operation = ""
@@ -45,8 +49,29 @@ numbers.forEach(number =>number.addEventListener('click',()=>{
         screenDisplay()    }
 }) )
 operators.forEach(operator => operator.addEventListener('click',()=>{
-    operation = `${operator.innerText}`
-    screenDisplay()
+    if (operation == '') {
+        operation = `${operator.innerText}`
+        screenDisplay()
+    }
+    else if (operation != '') {
+        operate(firstNumber,secondNumber)   
+        firstNumber = operate(firstNumber,secondNumber)
+        operation = `${operator.innerText}`
+        secondNumber = ''
+        screenDisplay()
+    }
 } 
 ))
-
+equals.addEventListener('click',()=>{
+    total = operate(firstNumber,secondNumber)
+    firstNumber = total
+    operation = ''
+    secondNumber = ''
+    screenDisplay()
+})
+clear.addEventListener('click',()=> {
+    firstNumber = ''
+    secondNumber = ''
+    operation = ''
+    screenDisplay()
+})
