4aee9b4f3fc5cb3b21111002245c38b7cae9b47d
Aymen Loudiy <123456789+Nimueh-Ilya@users.noreply.github.com>
2023-10-03 15:26:05 +0100

    Set screen display Restructure HTML skeleton Add event listeners to number and operator buttons Add user input to calculator display

--- index.js
+++ index.js
@@ -24,6 +24,29 @@ function operate(num1,num2,operator) {
         return division(num1,num2)
     }
 }
-const firstNumber = ""
-const secondNumber = ""
-const operation = ""
+const screen = document.querySelector('.screen')
+const buttons = document.querySelectorAll('.buttons')
+const numbers = document.querySelectorAll('.number')
+const operators = document.querySelectorAll('.operator')
+let firstNumber = ""
+let secondNumber = ""
+let operation = ""
+let total = 0
+function screenDisplay() {
+    screen.innerText = `${firstNumber} ${operation} ${secondNumber}`
+}
+numbers.forEach(number =>number.addEventListener('click',()=>{
+    if (operation == '') {
+        firstNumber = firstNumber + number.innerText
+        screenDisplay()
+    }
+    else if (operation != '') {
+        secondNumber = secondNumber+number.innerText
+        screenDisplay()    }
+}) )
+operators.forEach(operator => operator.addEventListener('click',()=>{
+    operation = `${operator.innerText}`
+    screenDisplay()
+} 
+))
+
